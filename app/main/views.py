5b1a023e6d3a52f174f3199573d034e67a89ff09
Mercy <mercycherotich757@gmail.com>
2022-02-27 19:50:56 +0300

    link source to articles

--- app/main/views.py
+++ app/main/views.py
@@ -19,6 +19,17 @@ def index():
     title = 'Welcome to news hour!'
     return render_template('index.html',title = title,general=general_news,health=health_news,technology=technology_news,business=business_news)
 
+@main.route('/articles/<source_id>')
+def articles(source_id):
+
+    '''
+    View source page function that returns the source articles page and its data
+    '''
+    articles = get_articles(source_id)
+    name = f'{articles.name}'
+
+    return render_template('articles.html',name=name,articles=articles)
+
 # @app.route('/source/<string:source_id>')
 # def source(source_id):
 
@@ -28,24 +39,14 @@ def index():
 #     id = "abc-news"
 #     return render_template('source.html',id = source_id)
 
-@main.route('/source/<string:id>')
-def source(id):
+# @main.route('/source/<string:id>')
+# def source(id):
 
-    '''
-    View source page function that returns the source articles page and its data
-    '''
-    source = get_source(id)
-    name = f'{source.name}'
+#     '''
+#     View source page function that returns the source articles page and its data
+#     '''
+#     source = get_source(id)
+#     name = f'{source.name}'
 
-    return render_template('source.html',name=name,source=source)
+#     return render_template('source.html',name=name,source=source)
 
-@main.route('/articles/<string:id>')
-def articles(id):
-
-    '''
-    View source page function that returns the source articles page and its data
-    '''
-    articles = get_articles(id)
-    name = f'{articles.name}'
-
-    return render_template('articles.html',name=name,articles=articles)
--- app/requests.py
+++ app/requests.py
@@ -4,13 +4,17 @@ from app.models import Articles,Source
 
 # Getting api key
 apiKey = None
-# Getting the movie base url
 base_url = None
+article_url = None
 
 def configure_request(app):
-    global apiKey,base_url
+    global apiKey,base_url,article_url
     apiKey = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
+    article_url = app.config['NEWS_ARTICLE_URL']
+
+
+
 
 def get_sources(category):
     '''
@@ -25,35 +29,9 @@ def get_sources(category):
             source_results_list = get_source_response['sources']
             source_results = process_results(source_results_list)
     return source_results
-def get_source(id):
-    get_source_details_url = base_url.format(id,apiKey)
-
-    with urllib.request.urlopen(get_source_details_url) as url:
-        source_details_data = url.read()
-        source_details_response = json.loads(source_details_data)
-        source_object = None
-        if source_details_response:
-            id = source_details_response.get('id')
-            name = source_details_response.get('name')
-            description = source_details_response.get('description')
-            url= source_details_response.get('url')
-            category = source_details_response.get('category')
-
-            source_object = Source(id,name,description,url,category)
-
-    return source_object
-
 
 def process_results(source_list):
-    '''
-    Function  that processes the movie result and transform them to a list of Objects
-
-    Args:
-        source_list: A list of dictionaries that contain movie details
-
-    Returns :
-        movie_results: A list of movie objects
-    '''
+    
     source_results = []
     for source_item in source_list:
         id = source_item.get('id')
@@ -67,14 +45,26 @@ def process_results(source_list):
 
     return source_results
 
+def get_source(id):
+    get_source_details_url = base_url.format(id,apiKey)
+    with urllib.request.urlopen(get_source_details_url) as url:
+        source_details_data = url.read()
+        source_details_response = json.loads(source_details_data)
+        source_object = None
+        if source_details_response:
+            id = source_details_response.get('id')
+            name = source_details_response.get('name')
+            description = source_details_response.get('description')
+            url= source_details_response.get('url')
+            category = source_details_response.get('category')
+            source_object = Source(id,name,description,url,category)
 
-def get_articles(id):
+    return source_object
+def get_article(id):
     '''
     A function that returns a list of articles
-    Arg:
-        source id 
     '''
-    get_articles_details_url = base_url.format(id,apiKey)
+    get_articles_details_url = article_url.format(id,apiKey)
     with urllib.request.urlopen(get_articles_details_url) as url:
         articles_details_data = url.read()
         articles_details_response = json.loads(articles_details_data)
@@ -87,7 +77,40 @@ def get_articles(id):
             urlToImage = articles_details_response.get('urlToImage')
             publishedAt = articles_details_response.get('publishedAt')
             content = articles_details_response.get('content')
-            
-            articles_object = Articles(title,description,url,urlToImage,publishedAt,content)
+        
+            articles_object = Articles(title,author,description,url,urlToImage,publishedAt,content)
 
     return articles_object
+
+
+def get_articles(source):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = article_url.format(source,apiKey)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+        articles_results = []
+        if get_articles_response['sources']:
+            articles_results_list = get_articles_response['sources']
+            articles_results = process_articles_results(articles_results_list)
+    return articles_results
+
+def process_articles_results(articles_list):
+    
+    articles_results = []
+    for articles_item in articles_list:
+        author = articles_item.get('author')
+        title = articles_item.get('title')
+        description = articles_item.get('description')
+        url= articles_item.get('url')
+        urlToImage = articles_item.get('urlToImage')
+        publishedAt = articles_item.get('publishedAt')
+        content = articles_item.get('content')
+        
+   
+        articles_object = Articles(author,title,description,url,urlToImage,publishedAt,content)
+        articles_results.append(articles_object)
+
+    return articles_results
